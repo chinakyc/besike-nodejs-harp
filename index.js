744853685aa87be792153ae85b7ffacbcef1d8db
Azul <chinakyc@qq.com>
2015-05-14 08:16:16 +0800

    Implement Preprocessors

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var connect = require('connect')
 var serverStatic = require('serve-static')
+var makeJade = require('./lib/processor/jade')
+var makeLess = require('./lib/processor/less')
 
 function _connect(dir) {
    var app = connect()
-   app.use(serverStatic(dir))
+   app
+     .use(serverStatic(dir))
+     .use(function(req, res, next) {
+        if (req.url === '/') {
+            req.url = '/index.html'
+	};
+	next()
+     })
+     .use(makeLess(dir))
+     .use(makeJade(dir))
    return app
 }
 
