192c3105007bd30f5bebcb070fe848f1b87e64a4
Azul <chinakyc@qq.com>
2015-05-16 10:32:10 +0800

    Mini Harp Preprocessors

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var connect = require('connect')
-var serverStatic = require('serve-static')
-var makeJade = require('./lib/processor/jade')
-var makeLess = require('./lib/processor/less')
+module.exports = _connect;
+var connect = require('connect');
+var serverStatic = require('serve-static');
+var makeJade = require('./lib/processor/jade');
+var makeLess = require('./lib/processor/less');
 
 function _connect(dir) {
-   var app = connect()
-   app
-     .use(serverStatic(dir))
-     .use(function(req, res, next) {
-        if (req.url === '/') {
-            req.url = '/index.html'
-	};
-	next()
-     })
-     .use(makeLess(dir))
-     .use(makeJade(dir))
-   return app
-}
+    var app = connect();
 
-module.exports = _connect
+    app
+        //.use(serverStatic(dir))
+        .use(function (req, res, next) {
+            if (req.url === '/') {
+                req.url = '/index.html';
+            }
+            next();
+        })
+        .use(makeLess(dir))
+        .use(makeJade(dir));
+    return app;
+}
