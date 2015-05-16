a90b3829c946ed3237676b2e8f28b1c3b573df19
Azul <chinakyc@qq.com>
2015-05-16 11:04:21 +0800

    misspelt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = _connect;
 var connect = require('connect');
+var path = require('path');
 var serverStatic = require('serve-static');
 var makeJade = require('./lib/processor/jade');
 var makeLess = require('./lib/processor/less');
@@ -8,7 +9,14 @@ function _connect(dir) {
     var app = connect();
 
     app
-        //.use(serverStatic(dir))
+		.use(function (req, res, next) {
+            extname = path.extname(req.url);
+            if (extname == '.jade' || extname == '.less') {
+                res.writeHead(404, {"Content-Type": 'text/plain'});
+				res.end();
+            }
+		    next();
+		})
         .use(function (req, res, next) {
             if (req.url === '/') {
                 req.url = '/index.html';
@@ -16,6 +24,7 @@ function _connect(dir) {
             next();
         })
         .use(makeLess(dir))
-        .use(makeJade(dir));
+        .use(makeJade(dir))
+        .use(serverStatic(dir));
     return app;
 }
